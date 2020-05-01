7c5e0b098051f58f2c0a0223f075752d47fd3706
KneeHunter <https://github.com/KneeHunter>
2020-05-01 23:02:24 +0900

    api

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: "d3becdd457a9f3e2f43d43c59968fa8d",
+        language: "en-US"
+    }
+});
+
+export const moviesApi = {
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular")
+}
+
+export const tvApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today")
+}
