8d93597cea1b80b93fca90db8744f802535b2319
KneeHunter <https://github.com/KneeHunter>
2020-05-03 15:27:40 +0900

    #5.6 Detail Container part Two

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,24 +1,46 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component{
-    state = {
-        result: null,
-        loading: true,
-        error: null
+    constructor(props){
+        super(props);
+        const { location: { pathname }} = props;
+        this.state = {
+            result: null,
+            loading: true,
+            error: null,
+            isMovie: pathname.includes("/movie/")
+        }
     }
 
     async componentDidMount(){
-        const {match: {params: {id}}, history: {push}} = this.props;
+        const {match: {params: { id }}, history: { push }} = this.props;
+        const { isMovie } = this.state;
         const parsedId = parseInt(id);
+        console.log(this.props);
         if(isNaN(parsedId)){
             return push("/");
         }
-        
+        let result = null;
+        try{
+            if(isMovie){
+                const request = await moviesApi.movieDetail(parsedId);
+                result = request.data;   
+            } else {
+                const request = await tvApi.showDetail(parsedId);
+                result = request.data;
+            }
+        } catch {
+            this.setState({ error: "Can't find anything." })
+        } finally {
+            this.setState({ loading: false, result })
+        }
     }
 
     render(){
-        const { result, loading, error } = this.state
+        const { result, loading, error } = this.state;
+        console.log(this.state);
         return(
             <DetailPresenter
                 result={result}
