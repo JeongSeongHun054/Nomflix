62ac519cab465585fa13e857173899f98ba79250
KneeHunter <https://github.com/KneeHunter>
2020-05-03 15:37:43 +0900

    #5.7 Destructing assignment with let

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -18,18 +18,15 @@ export default class extends React.Component{
         const {match: {params: { id }}, history: { push }} = this.props;
         const { isMovie } = this.state;
         const parsedId = parseInt(id);
-        console.log(this.props);
         if(isNaN(parsedId)){
             return push("/");
         }
         let result = null;
         try{
-            if(isMovie){
-                const request = await moviesApi.movieDetail(parsedId);
-                result = request.data;   
+            if( isMovie ){
+                ({data: result } = await moviesApi.movieDetail(parsedId));   
             } else {
-                const request = await tvApi.showDetail(parsedId);
-                result = request.data;
+                ({data: result } = await tvApi.showDetail(parsedId));
             }
         } catch {
             this.setState({ error: "Can't find anything." })
@@ -40,7 +37,7 @@ export default class extends React.Component{
 
     render(){
         const { result, loading, error } = this.state;
-        console.log(this.state);
+        console.log(result);
         return(
             <DetailPresenter
                 result={result}
